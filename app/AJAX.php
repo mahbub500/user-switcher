326a58234fa8d13caf762be18054bc975cf13e39
mahbub500 <mahbubmr500@gmail.com>
2024-10-27 21:57:59 +0600

    misc

--- app/AJAX.php
+++ app/AJAX.php
@@ -35,19 +35,18 @@ class AJAX extends Base {
 	    $search_term = isset($_POST['search']) ? sanitize_text_field($_POST['search']) : '';
 
 	    $args = [
-	        'search' => '*' . esc_attr($search_term) . '*',
+	        'search' => '*' . esc_attr( $search_term ) . '*',
 	        'search_columns' => ['user_login', 'user_email', 'display_name'],
 	        'fields' => ['ID', 'display_name'],
 	        'number' => -1, 
 	    ];
 
-	    $users = get_users($args);
+	    $users = get_users( $args );
 
-	    if ($users) {
-		    foreach ($users as $user) {
+	    if ( $users ) {
+		    foreach ( $users as $user ) {
 		        $switch_url = wp_nonce_url(add_query_arg('user_id', $user->ID, admin_url('index.php')), 'switch_to_user_' . $user->ID);
 		        
-		        // Correctly concatenate the strings and create a clickable link
 		        echo 'Name: ' . esc_html($user->display_name) . ' ID: ' . esc_html($user->ID) . ' <a href="' . esc_url($switch_url) . '">Go</a><br>';
 		    }
 		} else {
