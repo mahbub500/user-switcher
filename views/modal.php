0965a5b6ba8af052737bcd092ebb32bda6c61fcd
mahbub500 <mahbubmr500@gmail.com>
2024-11-24 16:21:08 +0600

    Change title modal

--- views/modal.php
+++ views/modal.php
@@ -3,7 +3,7 @@
  <div id="us-switcher-modal" style="display: none;">
     <div class="us-switcher-modal-content">
         <span class="us-switcher-close">&times;</span>	            
-        <h2><?php echo esc_html(__('User Switcher', 'switch-to-user')); ?></h2>
+        <h2><?php echo esc_html(__('Switch To User', 'switch-to-user')); ?></h2>
         <p><?php echo esc_html(__('Search users by name, display name, or email.', 'switch-to-user')); ?></p>
         <form id="us-switcher-form">
             <select class="us-user-name qlfv-user-onchange" id="user-info">
