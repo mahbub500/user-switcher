51451102893748db9843deb6ecf0cc275908c25f
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-12-04 19:58:54 +0600

    Text domain change

--- views/modal.php
+++ views/modal.php
@@ -3,13 +3,13 @@
  <div id="us-switcher-modal" style="display: none;">
     <div class="us-switcher-modal-content">
         <span class="us-switcher-close">&times;</span>	            
-        <h2><?php echo esc_html(__('Switch To User', 'user-switcher')); ?></h2>
-        <p><?php echo esc_html(__('Search users by name, display name, or email.', 'user-switcher')); ?></p>
+        <h2><?php echo esc_html(__('Switch To User', 'switch-to-user')); ?></h2>
+        <p><?php echo esc_html(__('Search users by name, display name, or email.', 'switch-to-user')); ?></p>
         <form id="us-switcher-form">
             <select class="us-user-name qlfv-user-onchange" id="user-info">
                 </select>
 			<p>
-				<input type="submit" id="us-switcher-button" value="<?php esc_html_e( 'Go', 'user-switcher' ); ?>" class="button button-primary " />
+				<input type="submit" id="us-switcher-button" value="<?php esc_html_e( 'Go', 'switch-to-user' ); ?>" class="button button-primary " />
             </p>
 		</form>
         <div id="us-switcher-results"></div>
